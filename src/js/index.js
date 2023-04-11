312e63be09ed6ae4d7726fd2bd4f7c98d73f95fe
Valerii Pozhydaiev <p_vf@i.ua>
2023-04-11 21:39:35 +0300

    state

--- src/js/index.js
+++ src/js/index.js
@@ -1,45 +1,93 @@
-import { Notify } from 'notiflix';
+import { Notify } from 'notiflix/build/notiflix-notify-aio';
+import { SearchQuery } from './searchQuery.js';
+
 import SimpleLightbox from 'simplelightbox';
 import 'simplelightbox/dist/simple-lightbox.min.css';
 
-import { SearchQuery } from './searchQuery.js';
+let page = 1;
+let query = '';
 
 const refs = {
   searchEl: document.querySelector('#search-form'),
   galleryEl: document.querySelector('.gallery'),
+  loadMoreEl: document.querySelector('.load-more'),
 };
 
 refs.searchEl.addEventListener('submit', onFormSubmit);
-
-let lightbox = new SimpleLightbox('.gallery a', {
-  /* options */
-});
+refs.loadMoreEl.addEventListener('click', onLoadMoreClick);
 
 function onFormSubmit(evt) {
   evt.preventDefault();
-  const query = evt.target.elements.searchQuery.value;
-  SearchQuery.onQuerySearch(query).then(res => {
-    const markup = res
-      .map(
-        el => `<a href="${el.largeImageURL}" class="photo-card">
-  <img src="${el.webformatURL}" alt="${el.tags}" loading="lazy" />
-  <div class="info">
-    <p class="info-item">
-      <b>Likes: ${el.likes}</b>
-    </p>
-    <p class="info-item">
-      <b>Views: ${el.views}</b>
-    </p>
-    <p class="info-item">
-      <b>Comments: ${el.comments}</b>
-    </p>
-    <p class="info-item">
-      <b>Downloads: ${el.downloads}</b>
-    </p>
-  </div>
-</a>`
-      )
-      .join('');
-    refs.galleryEl.insertAdjacentHTML('beforeend', markup);
+  query = evt.target.elements.searchQuery.value.trim();
+  if (query.length === 0) {
+    return;
+  }
+  page = 1;
+  evt.currentTarget.reset();
+  refs.galleryEl.innerHTML = '';
+  SearchQuery.onQuerySearch(query, page).then(data => {
+    if (data.length === 0) {
+      Notify.failure(
+        'Sorry, there are no images matching your search query. Please try again.'
+      );
+      return;
+    }
+    refs.galleryEl.insertAdjacentHTML('beforeend', onMarkup(data.hits));
+    refs.loadMoreEl.classList.remove('is-hidden');
   });
 }
+
+function onLoadMoreClick(evt) {
+  page += 1;
+  SearchQuery.onQuerySearch(query, page).then(data => {
+    console.log(data.totalHits);
+    refs.loadMoreEl.classList.remove('is-hidden');
+    refs.galleryEl.insertAdjacentHTML('beforeend', onMarkup(data.hits));
+  });
+}
+
+function onMarkup(data) {
+  let markup = data
+    .map(
+      el => `<div class="photo-item">
+                <a href="${el.largeImageURL}" class="photo-link">
+                  <div class="wrap">
+                    <img src="${el.webformatURL}" alt="${el.tags}" class="photo-image" loading="lazy" />
+                  </div>
+                    <div class="info">
+                    <p class="info-item">
+                      <b>Likes</b><br> ${el.likes}
+                    </p>
+                    <p class="info-item">
+                      <b>Views</b><br> ${el.views}
+                    </p>
+                    <p class="info-item">
+                      <b>Comments</b><br> ${el.comments}
+                    </p>
+                    <p class="info-item">
+                      <b>Downloads</b><br> ${el.downloads}
+                    </p>
+                  </div>
+                </a>
+              </div>`
+    )
+    .join('');
+  return markup;
+}
+
+let lightbox = new SimpleLightbox('.gallery a', {
+  captions: true,
+  captionSelector: 'img',
+  captionType: 'attr',
+  captionsData: 'alt',
+  captionPosition: 'bottom',
+  captionDelay: 250,
+  scrollZoom: false,
+});
+// console.dir(simpleLightbox);
+// lightbox.on('show.simplelightbox', function () {
+//   console.log(123); // do something…
+// });
+// gallery.on('error.simplelightbox', function (e) {
+//   console.log(e); // some usefull information
+// });
--- src/js/searchQuery.js
+++ src/js/searchQuery.js
@@ -1,30 +1,25 @@
-import axios, { Axios } from 'axios';
+import axios from 'axios';
 
 export class SearchQuery {
   static #URL = 'https://pixabay.com/api/';
   static #API_KEY = '35178730-35412664985809edebeff8fc3';
 
-  static onQuerySearch(query) {
+  static onQuerySearch(query, page) {
     const params = new URLSearchParams({
+      q: query,
       image_type: 'photo',
       orientation: 'horizontal',
       safesearch: true,
+      per_page: 40,
+      page,
     });
 
     const end_point = `${SearchQuery.#URL}?key=${
       SearchQuery.#API_KEY
-    }&q=${query}`;
+    }&${params}`;
 
-    return axios
-      .get(end_point, params)
-      .then(function (response) {
-        return response.data.hits;
-      })
-      .catch(function (error) {
-        console.log(error);
-      })
-      .finally(function () {
-        // always executed
-      });
+    return axios.get(end_point).then(function (response) {
+      return response.data;
+    });
   }
 }
